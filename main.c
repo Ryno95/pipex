5600c12e817a900ba75a74f00ed4f63be96aa74e
Ryno Meiboom <rmeiboom@f0r5s13.codam.nl>
2021-08-04 11:07:55 +0200

    tested env var, ready to start project

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define WRITE_FD 1
 #define CHILD_PROCESS_ID 0
 #define ERROR -1
-#define ALL_PERMISSIONS 0777
+#define PERMISSIONS 0644
 
 // ls -la | grep total
 
@@ -21,7 +21,7 @@
 // 	int	fd;
 // 	int	fd2;
 
-// 	fd = open("results.txt", O_WRONLY | O_CREAT, ALL_PERMISSIONS);
+// 	fd = open("results.txt", O_WRONLY | O_CREAT, PERMISSIONS);
 // 	if (fd == -1)
 // 		exit(3);
 		
@@ -48,52 +48,67 @@
 // 	return (1);
 // }
 
-int	main()
+// int	main()
+// {
+// 	int	fd[PIPE_BOTH_ENDS];
+// 	int	pid1;
+// 	int	pid2;
+
+
+// 	if (pipe(fd) == ERROR)
+// 		return (1);
+// 	pid1 = fork();
+// 	if (pid1 == ERROR)
+// 		return (2);
+// 	else if (pid1 == CHILD_PROCESS_ID)
+// 	{
+// 		// child process 1, execute first command ("ls -la" or "ping -c 5")
+// 		char *argv[] = {"/sbin/ping", "-c", "5", "google.com", NULL};
+// 		// char *lsargv[] = {"/bin/ls", "-la", NULL};
+// 		dup2(fd[WRITE_FD], STDOUT_FILENO);
+// 		close(fd[READ_FD]);
+// 		close(fd[WRITE_FD]);
+// 		if (execve(argv[0], argv, NULL) == ERROR)
+// 			printf("SHiiiiit\n");
+// 	}
+
+// 	pid2 = fork();
+// 	if (pid2 == ERROR)
+// 		return (3);
+// 	else if (pid2 == CHILD_PROCESS_ID)
+// 	{
+// 		// child process 2, execute second command ("grep round"in this case)
+// 		// char *argv[] = {"/bin/grep", "-e", NULL};
+// 		char *lsargv[] = {"/bin/ls", "-la", NULL};
+// 		dup2(fd[READ_FD], STDIN_FILENO);
+// 		close(fd[READ_FD]);
+// 		close(fd[WRITE_FD]);
+// 		char *env[] = {"PWD=", NULL};
+// 		if (execve(lsargv[0], lsargv, env) == ERROR)
+// 		// if (execlp("grep", "grep", "round", NULL) == ERROR)
+// 		{
+// 			printf("Well that's not right.....\n");
+// 			return (7);
+// 		}
+// 	}
+// 	close(fd[READ_FD]);
+// 	close(fd[WRITE_FD]);
+// 	waitpid(pid1, NULL, 0);
+// 	waitpid(pid2, NULL, 0);
+// 	return (0);
+// }
+
+
+void run(int argc, char *argv[], char *env[])
 {
-	int	fd[PIPE_BOTH_ENDS];
-	int	pid1;
-	int	pid2;
+	(void)argc;
+	(void)argv;
+	(void)env;
+	return ;
+}
 
-
-	if (pipe(fd) == ERROR)
-		return (1);
-	pid1 = fork();
-	if (pid1 == ERROR)
-		return (2);
-	else if (pid1 == CHILD_PROCESS_ID)
-	{
-		// child process 1, execute first command ("ls -la" or "ping -c 5")
-		char *argv[] = {"/sbin/ping", "-c", "5", "google.com", NULL};
-		// char *lsargv[] = {"/bin/ls", "-la", NULL};
-		dup2(fd[WRITE_FD], STDOUT_FILENO);
-		close(fd[READ_FD]);
-		close(fd[WRITE_FD]);
-		if (execve(argv[0], argv, NULL) == ERROR)
-			printf("SHiiiiit\n");
-	}
-
-	pid2 = fork();
-	if (pid2 == ERROR)
-		return (3);
-	else if (pid2 == CHILD_PROCESS_ID)
-	{
-		// child process 2, execute second command ("grep round"in this case)
-		// char *argv[] = {"/bin/grep", "-e", NULL};
-		char *lsargv[] = {"/bin/ls", "-la", NULL};
-		dup2(fd[READ_FD], STDIN_FILENO);
-		close(fd[READ_FD]);
-		close(fd[WRITE_FD]);
-		char *env[] = {"PWD=", NULL};
-		if (execve(lsargv[0], lsargv, env) == ERROR)
-		// if (execlp("grep", "grep", "round", NULL) == ERROR)
-		{
-			printf("Well that's not right.....\n");
-			return (7);
-		}
-	}
-	close(fd[READ_FD]);
-	close(fd[WRITE_FD]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+int main(int argc, char *argv[], char *env[])
+{
+	run(argc, argv, env);
 	return (0);
 }
