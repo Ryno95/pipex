1e8967f6296306e0cad4383797b270193b6f911c
Rynoster <Ryno.Meiboom@gmail.com>
2021-08-13 11:42:46 +0200

    Working version, refactor and test edge cases

--- srcs/.bonus/main.c
+++ srcs/.bonus/main.c
@@ -21,83 +21,6 @@ typedef struct s_in_and_outfile
 	int outfile;
 }	t_in_and_outfile;
 
-
-// t_multi_pipe_fd	*create_multiple_pipes(int num_of_pipes)
-// {
-// 	int					i;
-// 	t_multi_pipe_fd		**pipes;
-
-// 	i = 0;
-// 	pipes = (t_multi_pipe_fd **)ft_calloc(sizeof(t_multi_pipe_fd *), 1);
-// 	while (i < num_of_pipes)
-// 	{
-// 		pipes[i] = (t_multi_pipe_fd *)malloc(sizeof(t_multi_pipe_fd));
-// 		if (!pipes[i])
-// 			return (NULL);
-// 		if (pipe(pipes[i]->fd) == ERROR)
-// 			return (NULL);
-// 		i++;
-// 		printf("%d\n", i);
-// 	}
-// 	return (pipes);
-// }
-
-// void	close_un_used_fds(int number_of_pipes, t_multi_pipe_fd **pipe_fd, int process)
-// {
-// 	int j;
-
-// 	j = 0;
-// 	while (j < number_of_pipes)
-// 	{
-// 		if (process != j)
-// 			close((pipe_fd[j])->fd[READ_FD]);
-// 		if (process + 1 != j)
-// 			close(pipe_fd[j]->fd[WRITE_FD]);
-// 		j++;
-// 	}
-// }
-
-// int	fork_processes(int num_of_processes, int **pid, t_multi_pipe_fd **pipe_fd, int number_of_pipes)
-// {
-// 	int	i;
-// 	int	j;
-// 	for (i = 0; i < num_of_processes; i++)
-// 	{
-// 		pid[i] = fork();
-// 		if (pid[i] == -1)
-// 			printf("ERROR CREATING PROCESS");
-// 		if (pid[i] == 0)
-// 		{
-// 			// child process
-// 			close_un_used_fds(number_of_pipes, pipe_fd, i);
-// 			// do child thingies
-// 			//  here
-			
-// 			close(pipe_fd[i]->fd[READ_FD]);
-// 			close(pipe_fd[i + 1]->fd[WRITE_FD]);
-// 			return 0;
-// 		}
-// 	}
-// 	return (0);
-// }
-
-// int main(int argc, const char *argv[], const char *env[])
-// {
-// 	const int				num_of_processes = argc - 3;
-// 	const int				num_of_pipes = argc - 4;
-// 	t_multi_pipe_fd			**pipe_fd;
-// 	int						pid[MAX_PROCESSES];
-// 	(void)argv;
-// 	(void)env;
-// 	pipe_fd = create_multiple_pipes(num_of_pipes);
-// 	if (pipe_fd == NULL)
-// 		handle_errors(PIPE_ERROR, "Multipipe creator");
-// 	if (!fork_processes(num_of_processes, &pid[0], pipe_fd, num_of_pipes))
-// 		printf("you fucked uuup\n");
-// 	free(pipe_fd);
-// 	return (0);
-// }
-
 int	set_stdout(int new_std_out)
 {
 	return (dup2(new_std_out, STDOUT_FD));
@@ -108,50 +31,28 @@ int	set_stdin(int new_std_in)
 	return (dup2(new_std_in, STDIN_FD));
 }
 
-// int	bonus_child_process(int *fd, const char *argv[], const char *env[])
-// {
-// 	const int	infile = open(argv[1], O_RDONLY);
-// 	const char	*path_var = ft_get_env_var(env, PATH_ID);
-// 	const char	**cmd = (const char **)ft_split(argv[2], SPACE);
-// 	char		*path;
-
-// 	if (infile == ERROR || !cmd)
-// 		handle_errors(SAFETY, "child_process 29");
-// 	if (!redirect_stdin_and_stdout(infile, fd[WRITE_FD]))
-// 		handle_errors(FD_ERROR, "child_process redirecting fds");
-// 	close_multiple(fd[READ_FD], infile);
-// 	path = get_executable_path(path_var, cmd[0]);
-// 	if (!path)
-// 		handle_errors(MALLOC_ERROR, "child_process path allocation");
-// 	if (execute_command(path, cmd, env) == ERROR)
-// 		handle_errors(EXECUTION_ERROR, "child_process execute command");
-// 	return (1);
-// }
-
 void redirect_in_and_output(t_multi_pipes *pipes, t_in_and_outfile *files, int process, int num_of_processes)
 {
 	if (process == FIRST_PROCESS)
 	{
-		printf("First process in should be 0: %d\n", process);
 		if (set_stdin(files->infile) == ERROR)
-			handle_errors(FD_ERROR, "redirect, setting child output");
+			handle_errors(FD_ERROR, "redirect, setting child input");
+		close(files->infile);
 	}
 	else
 	{
-		printf("Second process in should be 1: %d\n", process);
 		if(set_stdin(pipes->previous[READ_FD]) == ERROR)
 			handle_errors(FD_ERROR, "redirect, setting child input");
 	}
 
 	if (process == num_of_processes - 1)
 	{
-		printf("Last process out should be 1: %d\n", process);
 		if (!set_stdout(files->outfile))
 			handle_errors(FD_ERROR,"redirect, setting output to file");
+		close(files->outfile);
 	}
 	else
 	{
-		printf("First process out should be 0: %d\n", process);
 		if (!set_stdout(pipes->current[WRITE_FD]))
 			handle_errors(FD_ERROR, "redirect, setting child output");
 	}
@@ -182,11 +83,11 @@ void	current_to_previous_pipe(t_multi_pipes *pipes)
 	pipes->previous[WRITE_FD] = pipes->current[WRITE_FD];
 }
 
-void	child(const char *argv[], const char *env[])
+void	child(const char *argv[], const char *env[], int process)
 {
- 	const char	*path_var = ft_get_env_var(env, PATH_ID);
- 	const char	**cmd = (const char **)ft_split(argv[2], SPACE);
- 	char		*path;
+	const char	*path_var = ft_get_env_var(env, PATH_ID);
+	const char	**cmd = (const char **)ft_split(argv[process + 2], SPACE);
+	char		*path;
 
 	if (!cmd)
 		handle_errors(SAFETY, "child, invalid command");
@@ -212,7 +113,7 @@ int main(int argc, const char *argv[], const char *env[])
 	for(int i = 0; i < num_of_processes; i++)
 	{
 		if (pipe(pipes.current) == ERROR)
-			handle_errors(PIPE_ERROR, "pipe curretn main");
+			handle_errors(PIPE_ERROR, "pipe current main");
 		pid[i] = fork();
 		if (pid[i] == ERROR)
 			handle_errors(ERROR, "forking main");
@@ -220,9 +121,10 @@ int main(int argc, const char *argv[], const char *env[])
 		{
 			redirect_in_and_output(&pipes, &files, i, num_of_processes);
 			close(pipes.current[READ_FD]);
-			printf("doing process number: %d\n", i + 1);
-			child(argv, env);
-			// do child stuff 
+			if (i > FIRST_PROCESS)
+				close(pipes.previous[WRITE_FD]);
+			close(pipes.current[READ_FD]);
+			child(argv, env, i);
 		}
 		// close fuuuuckiing fds fucker!
 		if (i != FIRST_PROCESS)
@@ -233,6 +135,8 @@ int main(int argc, const char *argv[], const char *env[])
 	close_multiple(pipes.current[READ_FD], pipes.current[WRITE_FD]);
 	close_multiple(pipes.previous[READ_FD], pipes.previous[WRITE_FD]);
 	close_multiple(files.infile, files.outfile);
-	while(wait(NULL));
+	printf("Finished all child processes\n");
+	for (int j = 0; j < num_of_processes; j++)
+		waitpid(pid[j], NULL, 0);
 	return (0);
 }
